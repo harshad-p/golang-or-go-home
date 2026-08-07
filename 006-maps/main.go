b912f61d3fc4082d5a58ea072988ba20aa764086
harshad-p <harshad.mscs@gmail.com>
2026-08-07 22:46:18 +0200

    feat: enhance Bob's favorite language handling in maps demo

--- 006-maps/main.go
+++ 006-maps/main.go
@@ -14,8 +14,14 @@ func main() {
 	// Print Harshad's favorite language
 	fmt.Println("Harshad's favorite language:", favoriteLanguages["Harshad"])
 
+	value, exists := favoriteLanguages["Bob"]
+
 	// Print Bob's favorite language
-	fmt.Println("Bob's favorite language:", favoriteLanguages["Bob"]) //empty-string
+	fmt.Println("Bob's favorite language:", value) //empty-string
+
+	if !exists {
+		fmt.Println("Bob's favorite language does not exist in the dictionary")
+	}
 
 	favoriteLanguages["Bob"] = "Python"
 	fmt.Println("After adding Bob's favorite language:", favoriteLanguages["Bob"])
