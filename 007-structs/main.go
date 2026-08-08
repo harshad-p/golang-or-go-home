f973d752fd2a53386aeda3369b4f5dcdfaaa7818
harshad-p <harshad.mscs@gmail.com>
2026-08-08 22:30:31 +0200

    Update struct field and print.

--- 007-structs/main.go
+++ 007-structs/main.go
@@ -12,9 +12,16 @@ func main() {
 	harryPotterBook := Book{
 		Title:  "Harry Potter and the Goblet of Fire",
 		Author: "J. K. Rowling",
-		Pages:  1000,
+		Pages:  100,
 	}
 
 	// Print a struct directly
 	fmt.Println(harryPotterBook)
+
+	// Print a struct field
+	fmt.Println(harryPotterBook.Pages)
+
+	// Modify value of a field
+	harryPotterBook.Pages = 700
+	fmt.Println(harryPotterBook.Pages)
 }
