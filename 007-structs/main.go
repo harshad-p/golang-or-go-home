10183d9929dbf4f3059b94f9fcef452bebec315f
harshad-p <harshad.mscs@gmail.com>
2026-08-09 10:00:16 +0200

    feat: sruct methods.

--- 007-structs/main.go
+++ 007-structs/main.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
-	type Book struct {
-		Title  string
-		Author string
-		Pages  int
-	}
+type Book struct {
+	Title  string
+	Author string
+	Pages  int
+}
 
+func (book Book) ToString() {
+	fmt.Println("Title\t\t:\t", book.Title)
+	fmt.Println("Author\t\t:\t", book.Author)
+	fmt.Println("Pages\t\t:\t", book.Pages)
+}
+
+func main() {
 	harryPotterBook := Book{
 		Title:  "Harry Potter and the Goblet of Fire",
 		Author: "J. K. Rowling",
@@ -24,4 +30,7 @@ func main() {
 	// Modify value of a field
 	harryPotterBook.Pages = 700
 	fmt.Println(harryPotterBook.Pages)
+
+	// Use connected method to print Book
+	harryPotterBook.ToString()
 }
